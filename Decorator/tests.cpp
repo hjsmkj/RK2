334f87f54a136a78b5eb5571b3d39708115358e6
hjsmkj <vitaliapavlovaa@mail.ru>
2024-09-07 17:58:32 +0300

    changed tests

--- Decorator/tests.cpp
+++ Decorator/tests.cpp
@@ -81,6 +81,3 @@ TEST(CompressionMessageDecoratorTest, Serialize) {
     std::string result = compressedMsg.Serialize();
     EXPECT_EQ(result, "ab"); // Зависит от реализации
 }
-int main() {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
